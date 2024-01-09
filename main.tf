3445cbc6f9b3bddacdbc5252fac543be3fda515a
leylamnavarro <147434480+leylamnavarro@users.noreply.github.com>
2024-01-09 12:38:54 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "autodeploy" {
-  key_name   = "autodeploy"  # Set a unique name for your key pair
+  #key_name   = "autodeploy"  # Set a unique name for your key pair
   public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
 }
 
@@ -11,5 +11,5 @@ resource "aws_instance" "public_instance" {
     Name = var.name_tag,
   }
   
-  #key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
+  key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
 }
