2a8cf6a8d54eabca76235835883efa55981544b7
leylamnavarro <leylamnavarro@gmail.com>
2024-01-10 20:10:58 -0800

    update9

--- main.tf
+++ main.tf
@@ -13,26 +13,29 @@ resource "aws_instance" "public_instance" {
   
   key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
 }
-#creating security groups to allow my teams IP's
+#creating a security group with terraform code that allows ssh access to only the members of my teams public IP’s
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = "vpc-0eeda1b52f1b8b78a"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = vpc-0eeda1b52f1b8b78a
+
   ingress {
-    description      = "SSH from VPC"
+    description      = "Ssh from VPC"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["71.202.208.227", "76.210.139.68"]
-    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    
   }
+
   egress {
     from_port        = 22
     to_port          = 22
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    #ipv6_cidr_blocks = ["::/0"]
+   
   }
+
   tags = {
     Name = "allow_ssh"
   }
