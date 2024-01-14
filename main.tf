a3093c1bf25efaf57c1719fdcba4ead7c3197470
leylamnavarro <leylamnavarro@gmail.com>
2024-01-13 20:59:15 -0800

    changed line 107

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ resource "aws_subnet" "private_subnets" {
 variable "azs" {
  type        = list(string)
  description = "Availability Zones"
- default     = ["us-east-1b"]
+ default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 }
 
 #subnet resource blocks used to map pub/priv subnet across avail zones
