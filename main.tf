4a0d680791781a4c70992eec3833dffc7c83ecf2
leylamnavarro <leylamnavarro@gmail.com>
2024-01-13 21:05:00 -0800

    test 3 failed running new test

--- main.tf
+++ main.tf
@@ -129,3 +129,25 @@ resource "aws_subnet" "private_subnets" {
    Name = "Private Subnet ${count.index + 1}"
  }
 }
+
+#trying to get an internet gateway
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
